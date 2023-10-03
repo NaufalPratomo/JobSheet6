8998414eebf75853100285ab9c63db0ad9f20f72
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-10-03 10:32:42 +0700

    modifikasi tahun kelipatan 100 & juga kelipatan400

--- Pemilihan2Percobaan119.java
+++ Pemilihan2Percobaan119.java
@@ -7,7 +7,10 @@ public class Pemilihan2Percobaan119 {
         System.out.print("Masukkan tahun : ");
         int tahun = input19.nextInt();
 
-        if ((tahun % 4) == 0) {
+        if ((tahun % 400) == 0) 
+        { if ((tahun % 100) == 0) {
+            System.out.println("Tahun Kabisat");}
+        } else if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.print("Tahun Kabisat");
                 else 
